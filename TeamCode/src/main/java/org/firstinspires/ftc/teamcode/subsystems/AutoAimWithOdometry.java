5073acba55cb74ab33fb7067e747dba310743725
crazyyen <ttln10@outlook.com>
2026-01-07 17:45:56 -0800

    remote

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AutoAimWithOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/AutoAimWithOdometry.java
@@ -1,17 +1,21 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.autonomous.Paths.autoAimPath;
 
 public class AutoAimWithOdometry {
 
+    private double alignHeadingAngle;
+
     private Follower follower;
 
     Pose blueGoalPose = new Pose(137.397, 142.394);
-    Pose redGoalPose = new Pose(6.603, 142.394);
     Pose startPose = new Pose(22,120, Math.toRadians(135));
     Pose currentPose;
 
@@ -21,17 +25,41 @@ public class AutoAimWithOdometry {
         follower.startTeleopDrive();
     }
 
-    private double slopeCalculator(double currentX, double currentY, double targetHeadingX, double targetHeadingY) {
-        return (currentY-targetHeadingY) / (currentX-targetHeadingX);
-    }
-
-    private double headingCalculator(double m1, double m2) {
+    private double headingCalculator() {
+        // m1 and m2 representing 2 different lines slopes
         // Formula: tan(theta) = abs((m1 - m2) / (1 + (m1)(m2)))
-        double tanTheta = Math.abs((m1-m2) / (1 + m1*m2));
+        // Answer will always be acute angle, next formula shown is the correct modified one
+
+        // Formula: tan(theta) = (m1 - m2) / (1 + (m1)(m2))
+        // This COULD result in a negative number which if it does, then take 180 - result
+
+        double robotX = follower.getPose().getX();
+        double robotY = follower.getPose().getY();
+
+        // Instead of slopes, we use the "Vector" between the two points
+        double m1 = (blueGoalPose.getY() - robotY) / (blueGoalPose.getX() - robotX);
+
+        double tanTheta = (m1-0) / (1 + m1*0);
         return Math.atan(tanTheta);
     }
 
-    private void setHeading() {
-        autoAimPath.getPath(follower, headingCalculator(0, slopeCalculator(follower.getPose().getX(), follower.getPose().getY(), blueGoalPose.getX(), blueGoalPose.getY())));
+    private double setHeading() {
+        double alignAngle = headingCalculator();
+
+        if (alignAngle < 0) {
+            alignHeadingAngle = (180 - alignAngle);
+        } else if (alignAngle > 0) {
+            alignHeadingAngle = alignAngle;
+        } else {
+            alignHeadingAngle = alignAngle;
+        }
+
+        return alignAngle;
     }
+
+    private void runAlignment() {
+        // Use PID for this. Should be similar to camera alignment.
+        PathChain path = autoAimPath.getPath(follower, setHeading());
+    }
+
 }
